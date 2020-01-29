f9bdfeae1c100bb03f20f5e0bb6b4238638d452a
Peter Finn <c16407722@mydit.ie>
2020-01-29 16:58:40 +0000

    Update Write files with randomised time.sh
    Added checks for if the clock sets correctly

--- Write files with randomised time.sh
+++ Write files with randomised time.sh
@@ -14,7 +14,7 @@ fi
 # check if correct number of arguements are given.
 if (( $# != 2 ))
 then
-	echo "Wrong number of arguements."
+	echo "Error: Wrong number of arguements."
 	echo "./program.sh /input/directory /output/directory"
 fi
 
@@ -38,6 +38,19 @@ sudo hwclock --set --date "$month/$day/$year $hour:$min:$sec"
 sudo hwclock -s
 }
 
+# check if the program can set the system time
+orgDate=$(date|awk '{print $1 $2 $3}')
+randomiseClock
+sleep 2
+setDate=$(date|awk '{print $1 $2 $3}')
+
+if [[ "$orgDate" == "$setDate" ]]
+then
+	echo "Error: Failed to set the hwclock. check if network time is turned on"
+	echo "Exiting."
+	exit
+fi
+
 # For some reason it occasionally fails to set the time
 # It will keep trying until the system accepts the time
 for file in $syncFrom/*
@@ -56,4 +69,4 @@ done
 
 ls -lt $syncTo
 echo ""
-echo "Confirm this worked correctly by comparing the dates written on the files"
+echo "Please confirm this worked correctly by comparing the dates written on the files above"
